ba956be5fb247dc16d865caf29f810978247b9dc
Brendan Long <self@brendanlong.com>
2020-08-17 12:33:52 -0600

    Handle resetting our wait time if the size of the pool changes

--- src/pool.ml
+++ src/pool.ml
@@ -10,6 +10,7 @@ type 'a item =
 
 type 'a t =
   { items : 'a item Deferred.t Uuid.Table.t
+  ; mutated : unit Condition.t
   ; max_elements : int
   ; expire_timeout : Time.Span.t
   ; new_item : unit -> 'a Deferred.t
@@ -28,6 +29,7 @@ let create
     ()
   =
   { items = Uuid.Table.create ()
+  ; mutated = Condition.create ()
   ; max_elements
   ; expire_timeout
   ; new_item
@@ -49,8 +51,16 @@ let close { items; on_empty; _ } =
 ;;
 
 let rec enqueue
-    ({ items; max_elements; expire_timeout; new_item; kill_item; check_item; on_empty; _ }
-    as t)
+    ({ items
+     ; mutated
+     ; max_elements
+     ; expire_timeout
+     ; new_item
+     ; kill_item
+     ; check_item
+     ; on_empty
+     ; _
+     } as t)
     f
   =
   let%bind item =
@@ -60,7 +70,7 @@ let rec enqueue
            let%map ({ value; _ } as item) = item in
            if Throttle.num_jobs_running value = 0 then Some item else None)
     >>= function
-    | Some item -> return item
+    | Some item -> return (`Item_available item)
     | None ->
       (* Add a new item if there's space in the queue and then use that *)
       if Hashtbl.length items < max_elements
@@ -72,6 +82,7 @@ let rec enqueue
          cleanup function *)
           Throttle.at_kill value (fun item ->
               Hashtbl.remove items key;
+              Condition.broadcast mutated ();
               Monitor.protect
                 (fun () -> kill_item item)
                 ~finally:(fun () ->
@@ -81,47 +92,53 @@ let rec enqueue
           { value; last_used_uuid = Uuid_unix.create () }
         in
         Hashtbl.add_exn items ~key ~data:item;
-        item)
+        Condition.broadcast mutated ();
+        let%map item = item in
+        `Item_available item)
       else
         (* If the queue is full, wait for one of the existing items or for the size to change *)
-        Hashtbl.data items
-        |> List.map ~f:(fun item ->
-               choice
-                 (let%bind item = item in
-                  Throttle.capacity_available item.value >>| fun () -> item)
-                 Fn.id)
+        choice (Condition.wait mutated) (fun () -> `Mutated)
+        :: (Hashtbl.data items
+           |> List.map ~f:(fun item ->
+                  choice
+                    (let%bind item = item in
+                     Throttle.capacity_available item.value >>| fun () -> item)
+                    (fun item -> `Item_available item)))
         |> Deferred.choose
   in
-  if Throttle.is_dead item.value
-  then Throttle.cleaned item.value >>= fun () -> enqueue t f
-  else if Throttle.num_jobs_running item.value > 0
-  then enqueue t f
-  else (
-    (* Update expiration UUID so other expiration processes don't delete this connection
+  match item with
+  | `Mutated -> enqueue t f
+  | `Item_available item ->
+    if Throttle.is_dead item.value
+    then Throttle.cleaned item.value >>= fun () -> enqueue t f
+    else if Throttle.num_jobs_running item.value > 0
+    then enqueue t f
+    else (
+      (* Update expiration UUID so other expiration processes don't delete this connection
        while we're using it *)
-    let uuid = Uuid_unix.create () in
-    item.last_used_uuid <- uuid;
-    let%bind res =
-      Throttle.enqueue item.value (fun value ->
-          if%bind check_item value
-          then (
-            let%map res = f value in
-            `Ok res)
-          else return `Check_failed)
-    in
-    match res with
-    | `Check_failed ->
-      Throttle.kill item.value;
-      Throttle.cleaned item.value >>= fun () -> enqueue t f
-    | `Ok res ->
-      (* Check expiration once the timeout finishes *)
-      let expires = Time.(add (now ()) expire_timeout) in
-      upon (at expires) (fun () ->
-          (* If the uuid doesn't match then some other process has used this item since the expiration process started *)
-          if Uuid.(item.last_used_uuid = uuid)
-          then (
-            (* UUID matches so nothing else has used this item; time to delete it *)
-            Log.Global.debug "Pool item expired";
-            Throttle.kill item.value));
-      return res)
+      let uuid = Uuid_unix.create () in
+      item.last_used_uuid <- uuid;
+      let%bind res =
+        Throttle.enqueue item.value (fun value ->
+            if%bind check_item value
+            then (
+              let%map res = f value in
+              `Ok res)
+            else return `Check_failed)
+      in
+      match res with
+      | `Check_failed ->
+        Throttle.kill item.value;
+        Throttle.cleaned item.value >>= fun () -> enqueue t f
+      | `Ok res ->
+        (* Check expiration once the timeout finishes *)
+        let expires = Time.(add (now ()) expire_timeout) in
+        upon (at expires) (fun () ->
+            (* If the uuid doesn't match then some other process has used this item since the expiration process started *)
+            if Uuid.(item.last_used_uuid = uuid)
+            then (
+              (* UUID matches so nothing else has used this item; time to delete it *)
+              Log.Global.debug "Pool item expired";
+              Throttle.kill item.value));
+        return res)
 ;;
