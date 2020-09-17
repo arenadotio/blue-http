0ea3c4fdde3127eebf4865f1e2c51f59e0077797
Anurag Soni <anurag@sonianurag.com>
2020-09-17 12:08:36 -0400

    address review comments

--- src/logger.ml
+++ src/logger.ml
@@ -5,12 +5,10 @@ let key = Univ_map.Key.create ~name:"logging_uuid" Uuid.sexp_of_t
 let with_uuid uuid f = Scheduler.with_local key (Some uuid) ~f
 
 let with_uuid_tags tags =
-  let open Option.Let_syntax in
-  let tag =
-    let%map uuid = Scheduler.find_local key in
-    "request_uuid", Uuid.to_string uuid
-  in
-  Option.(to_list tag @ value ~default:[] tags)
+  let existing_tags = Option.value ~default:[] tags in
+  match Scheduler.find_local key with
+  | None -> existing_tags
+  | Some uuid -> ("request_uuid", Uuid.to_string uuid) :: existing_tags
 ;;
 
 let info ?tags =
